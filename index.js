c52cf3a4dacfa6b31351a376bea4e013a68d330d
David Siaw <donkorsiawdavid@gmail.com>
2022-02-11 09:43:02 +0000

    Left with insert section

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const bodyParser= require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
 const app=express();
 const port=4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname  + '/public'));
+
+
+app.set("view engine",'ejs');
 
 app.get('/', (req,res)=>{
-    res.send("siaw");
+    res.render("pages/index");
 })
+
+app.get('/buyCrypto', (req,res)=>{
+    res.render("pages/buycrypto");
+});
+
+app.post('/buyCrypto', (req,res)=>{
+    //res.render("pages/buycrypto");
+
+})
+
+app.get('/transactions', (req,res)=>{
+    
+        const db =new sqlite3.Database('./db/myCryptoBank.db',sqlite3.OPEN_READWRITE,(err)=>{
+                if(err){
+                    console.error(err.message)
+                }
+                console.log("Connected to the Transactions DB");
+            });
+
+            db.all("select * from transactions",[],(err,rows)=>{
+                if(err){
+                    console.error(err.message);
+                }
+                res.render("pages/transactions",{transactions:rows.sort(function(a,b){
+                    return b.time-a.time;
+                })}); 
+            })
+        console.log("no data found");
+         
+});
+
+
+
 app.listen(port,()=>{
 console.log("Server starting at port 4000");
 })
